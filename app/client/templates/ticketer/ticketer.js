1b746e85bd1e723b1210eafa79eb2f43a21668e1
megatricycle <megatricycle@gmail.com>
2015-05-01 19:50:39 +0800

    Calls to create ticket with category, type, and name

--- app/client/templates/ticketer/ticketer.js
+++ app/client/templates/ticketer/ticketer.js
@@ -2,9 +2,13 @@
 /* Ticketer: Event Handlers */
 /*****************************************************************************/
 Template.Ticketer.events({
-  'click button': function(){
+  'click button': function(e){
     //@TODO: authenticate the type
-    Meteor.call("createTicket");
+    var category = $(e.target).closest("li[data-category]").data("category"),
+        type = $(e.target).closest("li[data-type]").data("type"),
+        name = $(e.target).data("name");
+
+    Meteor.call("createTicket", category, type, name);
   }
 });
 
--- app/server/methods.js
+++ app/server/methods.js
@@ -2,7 +2,10 @@
 /* Server Only Methods */
 /*****************************************************************************/
 Meteor.methods({
-  'createTicket': function(){
-    console.log("Creating ticket!");
+  'createTicket': function(category, type, name){
+    console.log("Creating new ticket..");
+    console.log("Category: "+category);
+    console.log("Type: "+type);
+    console.log("Name: "+name);
   }
 });
